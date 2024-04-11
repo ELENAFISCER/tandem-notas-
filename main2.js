ce8b3bf4aee2905fc8e1cdf004a77e404001755f
elenafiscer <tandem.fiscer@gmail.com>
2024-04-11 13:35:01 +0200

    empiezo logica

---
+++ main2.js
@@ -0,0 +1,21 @@
+// recoger la informacion
+function getInfo(){
+// devuelve el texto
+  console.log('texto ingresado')
+  return 'texto ingresado'
+}
+// almacenarla en localstorage
+function setLocalInfo(){
+//guardaría la info en localStorage
+return true;
+
+}
+// sacarla por pantalla (leerla)
+function showInfo(){
+  //si no hay notas mostrar no hay nada
+return true; 
+}
+// limpiarla
+function cleanInfo(){
+  //limpiar el local storage y limpiar la pantalla 
+}
